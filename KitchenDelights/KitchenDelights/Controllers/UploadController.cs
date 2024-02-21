a0afbf4d72a28f8d68c22910ee5058b0593639aa
TungDSHE150203 <tungdshe150203@fpt.edu.vn>
2024-02-21 09:27:59 +0700

    Add document uploader API

--- KitchenDelights/KitchenDelights/Controllers/UploadController.cs
+++ KitchenDelights/KitchenDelights/Controllers/UploadController.cs
@@ -8,7 +8,7 @@ namespace KitchenDelights.Controllers
     [ApiController]
     public class UploadController : ControllerBase
     {
-        private readonly string _folderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images/");
+        private readonly string _folderPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/");
         
         [HttpPost]
         public async Task<IActionResult> Image(IFormFile image, string type)
@@ -25,22 +25,48 @@ namespace KitchenDelights.Controllers
                 return BadRequest("Wrong image type!");
             }
 
-            string subFolder = Path.Combine(_folderPath, _type.ToString());
+            string subfolder = Path.Combine(_folderPath, "images", _type.ToString());
 
-            if (!Directory.Exists(subFolder))
+            if (!Directory.Exists(subfolder))
             {
-                Directory.CreateDirectory(subFolder);
+                Directory.CreateDirectory(subfolder);
             }
 
             string filename = image.FileName;
 
             string uniqueName = WebEncoders.Base64UrlEncode(Guid.NewGuid().ToByteArray()) + filename;
 
-            var imagePath = Path.Combine(subFolder, uniqueName);
+            var imagePath = Path.Combine(subfolder, uniqueName);
 
             await image.CopyToAsync(new FileStream(imagePath, FileMode.Create));
 
             return Ok($"{Request.Scheme}://{Request.Host.Value}/images/{_type}/{uniqueName}");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Document(IFormFile document)
+        {
+            if(!IsDocument(document))
+            {
+                return BadRequest("Please upload a document!");
+            }
+
+            string subfolder = Path.Combine(_folderPath, "documents");
+
+            if (!Directory.Exists(subfolder))
+            {
+                Directory.CreateDirectory(subfolder);
+            }
+
+            string filename = document.FileName;
+
+            string uniqueName = WebEncoders.Base64UrlEncode(Guid.NewGuid().ToByteArray()) + filename;
+
+            var documentPath = Path.Combine(subfolder, uniqueName);
+
+            await document.CopyToAsync(new FileStream(documentPath, FileMode.Create));
+
+            return Ok($"{Request.Scheme}://{Request.Host.Value}/documents/{uniqueName}");
         }
 
         public static bool IsImage(IFormFile file)
@@ -57,6 +83,27 @@ namespace KitchenDelights.Controllers
                 && !Path.GetExtension(file.FileName).Equals(".png", StringComparison.CurrentCultureIgnoreCase)
                 && !Path.GetExtension(file.FileName).Equals(".webp", StringComparison.CurrentCultureIgnoreCase)
                 && !Path.GetExtension(file.FileName).Equals(".jpeg", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool IsDocument(IFormFile file)
+        {
+            if (!file.ContentType.Equals("application/msword", StringComparison.CurrentCultureIgnoreCase)
+                && !file.ContentType.Equals("application/vnd.openxmlformats-officedocument.wordprocessingml.document", StringComparison.CurrentCultureIgnoreCase)
+                && !file.ContentType.Equals("application/vnd.oasis.opendocument.text", StringComparison.CurrentCultureIgnoreCase)
+                && !file.ContentType.Equals("application/pdf", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return false;
+            }
+
+            if (!Path.GetExtension(file.FileName).Equals(".doc", StringComparison.CurrentCultureIgnoreCase)
+                && !Path.GetExtension(file.FileName).Equals(".docx", StringComparison.CurrentCultureIgnoreCase)
+                && !Path.GetExtension(file.FileName).Equals(".odt", StringComparison.CurrentCultureIgnoreCase)
+                && !Path.GetExtension(file.FileName).Equals(".pdf", StringComparison.CurrentCultureIgnoreCase))
             {
                 return false;
             }
