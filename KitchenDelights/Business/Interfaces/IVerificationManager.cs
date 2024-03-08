d36be3b0489814ce7837644ba70d9b5c6bc01a32
TungDSHE150203 <tungdshe150203@fpt.edu.vn>
2024-03-08 08:05:59 +0700

    Add initial Verification Repo and Manager

---
+++ KitchenDelights/Business/Interfaces/IVerificationManager.cs
@@ -0,0 +1,16 @@
+﻿using Business.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Interfaces
+{
+    public interface IVerificationManager
+    {
+        Task<List<VerificationDTO>> GetVerifications();
+        Task<bool> CreateVerification(VerificationDTO verification);
+        Task<bool> UpdateVerification(VerificationDTO verification);
+    }
+}
