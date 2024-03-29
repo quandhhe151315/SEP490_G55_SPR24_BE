5f33318a41ee8f9e0aa3de325dac9a83b34863a9
tannmhe160735 <105600279+tannmhe160735@users.noreply.github.com>
2024-03-29 16:57:49 +0700

    Update RecipeController.cs

--- KitchenDelights/KitchenDelights/Controllers/RecipeController.cs
+++ KitchenDelights/KitchenDelights/Controllers/RecipeController.cs
@@ -23,27 +23,27 @@ namespace KitchenDelights.Controllers
             _recipeManager = recipeManager;
         }
 
-        [HttpGet]
-        public async Task<IActionResult> GetAllRecipe()
-        {
-            List<RecipeDTO> recipes = [];
-            try
-            {
-                recipes = await _recipeManager.GetRecipes();
-                if (recipes.Count <= 0)
-                {
-                    return NotFound("There are not exist any recipe in database");
-                }
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
-            }
-            return Ok(recipes);
-        }
+        //[HttpGet]
+        //public async Task<IActionResult> GetAllRecipe()
+        //{
+        //    List<RecipeDTO> recipes = [];
+        //    try
+        //    {
+        //        recipes = await _recipeManager.GetRecipes();
+        //        if (recipes.Count <= 0)
+        //        {
+        //            return NotFound("There are not exist any recipe in database");
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        //    }
+        //    return Ok(recipes);
+        //}
 
         [HttpGet]
-        public async Task<IActionResult> FilterRecipe(string? searchName, int? category, int? country, int? ingredient, int? isfree, string? orderby, string? sort)
+        public async Task<IActionResult> GetAllRecipe(string? searchName, int? category, int? country, int? ingredient, int? isfree, string? orderby, string? sort)
         {
             List<RecipeDTO> recipes = [];
             try
@@ -61,152 +61,152 @@ namespace KitchenDelights.Controllers
             return Ok(recipes);
         }
 
-        [HttpGet]
-        public async Task<IActionResult> GetAllRecipeDESCbyRating()
-        {
-            List<RecipeDTO> recipes = [];
-            try
-            {
-                recipes = await _recipeManager.GetRecipesDESC();
-                if (recipes.Count <= 0)
-                {
-                    return NotFound("There are not exist any recipe in database");
-                }
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
-            }
-            return Ok(recipes);
-        }
+        //[HttpGet]
+        //public async Task<IActionResult> GetAllRecipeDESCbyRating()
+        //{
+        //    List<RecipeDTO> recipes = [];
+        //    try
+        //    {
+        //        recipes = await _recipeManager.GetRecipesDESC();
+        //        if (recipes.Count <= 0)
+        //        {
+        //            return NotFound("There are not exist any recipe in database");
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        //    }
+        //    return Ok(recipes);
+        //}
 
-        [HttpGet]
-        public async Task<IActionResult> GetAllRecipeASCbyRating()
-        {
-            List<RecipeDTO> recipes = [];
-            try
-            {
-                recipes = await _recipeManager.GetRecipesASC();
-                if (recipes.Count <= 0)
-                {
-                    return NotFound("There are not exist any recipe in database");
-                }
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
-            }
-            return Ok(recipes);
-        }
+        //[HttpGet]
+        //public async Task<IActionResult> GetAllRecipeASCbyRating()
+        //{
+        //    List<RecipeDTO> recipes = [];
+        //    try
+        //    {
+        //        recipes = await _recipeManager.GetRecipesASC();
+        //        if (recipes.Count <= 0)
+        //        {
+        //            return NotFound("There are not exist any recipe in database");
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        //    }
+        //    return Ok(recipes);
+        //}
 
-        [HttpGet]
-        public async Task<IActionResult> GetAllRecipeFree()
-        {
-            List<RecipeDTO> recipes = [];
-            try
-            {
-                recipes = await _recipeManager.GetRecipeFree();
-                if (recipes.Count <= 0)
-                {
-                    return NotFound("There are not exist any recipe in database");
-                }
-                foreach (RecipeDTO recipe in recipes)
-                {
-                    if (recipe.IsFree)
-                    {
-                        recipe.RecipePrice = 0;
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
-            }
-            return Ok(recipes);
-        }
+        //[HttpGet]
+        //public async Task<IActionResult> GetAllRecipeFree()
+        //{
+        //    List<RecipeDTO> recipes = [];
+        //    try
+        //    {
+        //        recipes = await _recipeManager.GetRecipeFree();
+        //        if (recipes.Count <= 0)
+        //        {
+        //            return NotFound("There are not exist any recipe in database");
+        //        }
+        //        foreach (RecipeDTO recipe in recipes)
+        //        {
+        //            if (recipe.IsFree)
+        //            {
+        //                recipe.RecipePrice = 0;
+        //            }
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        //    }
+        //    return Ok(recipes);
+        //}
 
-        [HttpGet]
-        public async Task<IActionResult> GetAllRecipePaid()
-        {
-            List<RecipeDTO> recipes = [];
-            try
-            {
-                recipes = await _recipeManager.GetRecipePaid();
-                if (recipes.Count <= 0)
-                {
-                    return NotFound("There are not exist any recipe in database");
-                }
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
-            }
-            return Ok(recipes);
-        }
+        //[HttpGet]
+        //public async Task<IActionResult> GetAllRecipePaid()
+        //{
+        //    List<RecipeDTO> recipes = [];
+        //    try
+        //    {
+        //        recipes = await _recipeManager.GetRecipePaid();
+        //        if (recipes.Count <= 0)
+        //        {
+        //            return NotFound("There are not exist any recipe in database");
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        //    }
+        //    return Ok(recipes);
+        //}
 
-        [HttpGet]
-        public async Task<IActionResult> GetAllRecipeByTitle(string? title)
-        {
-            List<RecipeDTO> recipes = [];
-            try
-            {
-                if (string.IsNullOrEmpty(title))
-                {
-                    recipes = await _recipeManager.GetRecipes();
-                }
-                else
-                {
-                    recipes = await _recipeManager.GetRecipeByTitle(title);
-                }
-                if (recipes.Count <= 0)
-                {
-                    return NotFound("Recipe not exist");
-                }
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
-            }
-            return Ok(recipes);
-        }
+        //[HttpGet]
+        //public async Task<IActionResult> GetAllRecipeByTitle(string? title)
+        //{
+        //    List<RecipeDTO> recipes = [];
+        //    try
+        //    {
+        //        if (string.IsNullOrEmpty(title))
+        //        {
+        //            recipes = await _recipeManager.GetRecipes();
+        //        }
+        //        else
+        //        {
+        //            recipes = await _recipeManager.GetRecipeByTitle(title);
+        //        }
+        //        if (recipes.Count <= 0)
+        //        {
+        //            return NotFound("Recipe not exist");
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        //    }
+        //    return Ok(recipes);
+        //}
 
-        [HttpGet]
-        public async Task<IActionResult> GetAllRecipeByCategory(int category)
-        {
-            List<RecipeDTO> recipes = [];
-            try
-            {
-                recipes = await _recipeManager.GetRecipeByCategory(category);
-                if (recipes.Count <= 0)
-                {
-                    return NotFound("Recipe not exist");
-                }
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
-            }
-            return Ok(recipes);
-        }
+        //[HttpGet]
+        //public async Task<IActionResult> GetAllRecipeByCategory(int category)
+        //{
+        //    List<RecipeDTO> recipes = [];
+        //    try
+        //    {
+        //        recipes = await _recipeManager.GetRecipeByCategory(category);
+        //        if (recipes.Count <= 0)
+        //        {
+        //            return NotFound("Recipe not exist");
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        //    }
+        //    return Ok(recipes);
+        //}
 
-        [HttpGet]
-        public async Task<IActionResult> GetAllRecipeByCountry(int country)
-        {
-            List<RecipeDTO> recipes = [];
-            try
-            {
-                recipes = await _recipeManager.GetRecipeByCountry(country);
-                if (recipes.Count <= 0)
-                {
-                    return NotFound("Recipe not exist");
-                }
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
-            }
-            return Ok(recipes);
-        }
+        //[HttpGet]
+        //public async Task<IActionResult> GetAllRecipeByCountry(int country)
+        //{
+        //    List<RecipeDTO> recipes = [];
+        //    try
+        //    {
+        //        recipes = await _recipeManager.GetRecipeByCountry(country);
+        //        if (recipes.Count <= 0)
+        //        {
+        //            return NotFound("Recipe not exist");
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        //    }
+        //    return Ok(recipes);
+        //}
 
         [HttpGet]
         public async Task<IActionResult> GetRecipeById(int recipeId)
