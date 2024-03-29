1a6411ec61db1910d6e484c2c637181914694b9e
TungDSHE150203 <tungdshe150203@fpt.edu.vn>
2024-03-29 09:12:54 +0700

    Update UserManagerTest.cs
    Add Unit test for CreateUser

--- KitchenDelights/Business.Test/UserManagerTest.cs
+++ KitchenDelights/Business.Test/UserManagerTest.cs
@@ -5,8 +5,8 @@ using Business.Managers;
 using Business.Profiles;
 using Data.Entity;
 using Data.Interfaces;
-using Moq;
 using FluentAssertions;
+using Moq;
 
 
 namespace Business.Test
@@ -29,6 +29,108 @@ namespace Business.Test
         }
 
         [Fact]
+        public async void CreateUser_CreateWithRegisterDTO_UserNotExistInRepo()
+        {
+            var users = UsersSample();
+            RegisterRequestDTO register = new()
+            {
+                Username = "register",
+                Email = "register@mail.com",
+                Password = "3A7A4EE1E7AE7D0C9F95193571200B157A6BE0FFA59EDC8F84D39E4AD2D4D9FE:C433EF5F9226A6E7DE52883236AC00EF:301200:SHA256",
+                StatusId = 1,
+                RoleId = 5
+            };
+            _userRepositoryMock.Setup(x => x.CreateUser(It.IsAny<User>())).Callback<User>(users.Add);
+            _userRepositoryMock.Setup(x => x.GetUser("register@mail.com")).ReturnsAsync(users.FirstOrDefault(user => user.Email.Equals("register@mail.com")));
+
+            IUserManager _userManager = new UserManager(_userRepositoryMock.Object, _mapper);
+            var boolResult = await _userManager.CreateUser(register);
+            var countResult = users.Count;
+
+            boolResult.Should().BeTrue();
+            countResult.Should().Be(4);
+        }
+
+        [Fact]
+        public async void CreateUser_NotCreateWithRegisterDTO_UserExistInRepo()
+        {
+            var users = UsersSample();
+            RegisterRequestDTO register = new()
+            {
+                Username = "duplicate",
+                Email = "mock2@mail.com",
+                Password = "3A7A4EE1E7AE7D0C9F95193571200B157A6BE0FFA59EDC8F84D39E4AD2D4D9FE:C433EF5F9226A6E7DE52883236AC00EF:301200:SHA256",
+                StatusId = 1,
+                RoleId = 5
+            };
+            _userRepositoryMock.Setup(x => x.CreateUser(It.IsAny<User>())).Callback<User>(users.Add);
+            _userRepositoryMock.Setup(x => x.GetUser("mock2@mail.com")).ReturnsAsync(users.FirstOrDefault(user => user.Email.Equals("mock2@mail.com")));
+
+            IUserManager _userManager = new UserManager(_userRepositoryMock.Object, _mapper);
+            var boolResult = await _userManager.CreateUser(register);
+            var countResult = users.Count;
+
+            boolResult.Should().BeFalse();
+            countResult.Should().Be(3);
+        }
+
+        [Fact]
+        public async void CreateUser_CreateWithCreateUserDTO_UserNotExistInRepo()
+        {
+            var users = UsersSample();
+            CreateUserDTO register = new()
+            {
+                Username = "register",
+                FirstName = "firstname",
+                MiddleName = "middlename",
+                LastName = "lastname",
+                Email = "create@mail.com",
+                Phone = "0904285035",
+                Avatar = "avatar",
+                Password = "3A7A4EE1E7AE7D0C9F95193571200B157A6BE0FFA59EDC8F84D39E4AD2D4D9FE:C433EF5F9226A6E7DE52883236AC00EF:301200:SHA256",
+                StatusId = 1,
+                RoleId = 5
+            };
+            _userRepositoryMock.Setup(x => x.CreateUser(It.IsAny<User>())).Callback<User>(users.Add);
+            _userRepositoryMock.Setup(x => x.GetUser("create@mail.com")).ReturnsAsync(users.FirstOrDefault(user => user.Email.Equals("create@mail.com")));
+
+            IUserManager _userManager = new UserManager(_userRepositoryMock.Object, _mapper);
+            var boolResult = await _userManager.CreateUser(register);
+            var countResult = users.Count;
+
+            boolResult.Should().BeTrue();
+            countResult.Should().Be(4);
+        }
+
+        [Fact]
+        public async void CreateUser_NotCreateWithCreateUserDTO_UserNotExistInRepo()
+        {
+            var users = UsersSample();
+            CreateUserDTO register = new()
+            {
+                Username = "register",
+                FirstName = "firstname",
+                MiddleName = "middlename",
+                LastName = "lastname",
+                Email = "mock3@mail.com",
+                Phone = "0904285035",
+                Avatar = "avatar",
+                Password = "3A7A4EE1E7AE7D0C9F95193571200B157A6BE0FFA59EDC8F84D39E4AD2D4D9FE:C433EF5F9226A6E7DE52883236AC00EF:301200:SHA256",
+                StatusId = 1,
+                RoleId = 5
+            };
+            _userRepositoryMock.Setup(x => x.CreateUser(It.IsAny<User>())).Callback<User>(users.Add);
+            _userRepositoryMock.Setup(x => x.GetUser("mock3@mail.com")).ReturnsAsync(users.FirstOrDefault(user => user.Email.Equals("mock3@mail.com")));
+
+            IUserManager _userManager = new UserManager(_userRepositoryMock.Object, _mapper);
+            var boolResult = await _userManager.CreateUser(register);
+            var countResult = users.Count;
+
+            boolResult.Should().BeFalse();
+            countResult.Should().Be(3);
+        }
+
+        [Fact]
         //Naming convention is MethodName_expectedBehavior_StateUnderTest
         public async void GetUser_GetUserById_UserExistInRepo()
         {
@@ -36,16 +138,17 @@ namespace Business.Test
             var users = UsersSample();
             List<UserDTO> userDTOs = [];
             userDTOs.AddRange(users.Select(_mapper.Map<User, UserDTO>));
-            _userRepositoryMock.Setup(x => x.GetUser(1)).ReturnsAsync(UsersSample().Find(user => user.UserId == 1)); //Mock User repository GetUser(int id) method
+            _userRepositoryMock.Setup(x => x.GetUser(1)).ReturnsAsync(users.Find(user => user.UserId == 1)); //Mock User repository GetUser(int id) method
 
             //Act
             IUserManager _userManager = new UserManager(_userRepositoryMock.Object, _mapper);
             var result = await _userManager.GetUser(1);
             var actual = userDTOs.Find(user => user.UserId == 1);
-            
+
             //Assert (using FluentAssertions)
             result.Should().NotBeNull().And.BeOfType<UserDTO>().And.BeEquivalentTo(actual!);
         }
+
 
         private static List<User> UsersSample()
         {
