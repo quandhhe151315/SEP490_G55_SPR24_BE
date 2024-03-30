59881ded23b4a1ed42a05d687db0632a4685b14e
TungDSHE150203 <tungdshe150203@fpt.edu.vn>
2024-03-30 18:47:42 +0700

    Add Marketplace Repository

---
+++ KitchenDelights/Data/Interfaces/IMarketplaceRepository.cs
@@ -0,0 +1,18 @@
+﻿using Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Interfaces
+{
+    public interface IMarketplaceRepository
+    {
+        Task<Marketplace?> GetMarketplace(int id);
+        Task<List<Marketplace>> GetMarketplaces();
+        void CreateMarketplace(Marketplace marketplace);
+        void UpdateMarketplace(Marketplace marketplace);
+        void Save();
+    }
+}
