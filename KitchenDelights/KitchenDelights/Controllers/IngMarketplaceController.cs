71b600bd845a4cdbabe5ebeb3a301860f5caecc2
TungDSHE150203 <tungdshe150203@fpt.edu.vn>
2024-04-08 19:36:41 +0700

    Add Unit Test cases for IngMarketplaceController

--- KitchenDelights/KitchenDelights/Controllers/IngMarketplaceController.cs
+++ KitchenDelights/KitchenDelights/Controllers/IngMarketplaceController.cs
@@ -3,6 +3,7 @@ using Business.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace KitchenDelights.Controllers
 {
@@ -36,6 +37,9 @@ namespace KitchenDelights.Controllers
         [HttpPost]
         public async Task<IActionResult> Create(IngredientMarketplaceDTO dto)
         {
+            if(dto.IngredientId < 0) return BadRequest("Invalid ingredient Id");
+            if(dto.MarketplaceId < 0) return BadRequest("Invalid marketplace Id");
+            if(dto.MarketplaceLink.IsNullOrEmpty()) return BadRequest("Link must not be empty!");
             bool isCreated = await _manager.CreateIngredientMarketplace(dto);
             return isCreated ? Ok("Create sucessful") : StatusCode(500, "Create ingredient-marketplace failed!");
         }
@@ -43,6 +47,9 @@ namespace KitchenDelights.Controllers
         [HttpPatch]
         public async Task<IActionResult> Update(IngredientMarketplaceDTO dto)
         {
+            if(dto.IngredientId < 0) return BadRequest("Invalid ingredient Id");
+            if(dto.MarketplaceId < 0) return BadRequest("Invalid marketplace Id");
+            if(dto.MarketplaceLink.IsNullOrEmpty()) return BadRequest("Link must not be empty!");
             bool isUpdated = await _manager.UpdateIngredientMarketplace(dto);
             return isUpdated ? Ok() : NotFound("Ingredient-Marketplace doesn't exist!");
         }
@@ -50,6 +57,8 @@ namespace KitchenDelights.Controllers
         [HttpDelete]
         public async Task<IActionResult> Delete(int ingredientId, int marketplaceId)
         {
+            if(ingredientId < 0) return BadRequest("Invalid ingredient Id");
+            if(marketplaceId < 0) return BadRequest("Invalid marketplace Id");
             bool isDeleted = await _manager.DeleteIngredientMarketplace(ingredientId, marketplaceId);
             return isDeleted ? Ok() : NotFound("Ingredient-Marketplace doesn't exist!");
         }
